6ca78c48def4094baeae74fb05e55ae60b0b35ba
VitaliyDmitrienko <vddjack2008@yandex.ru>
2023-08-27 15:13:44 +0300

    2nd. Change pom.xml to trying resolve un-create annotated processor. Why it's not created?

--- src/main/java/org/example/Author.java
+++ src/main/java/org/example/Author.java
@@ -7,15 +7,15 @@ public class Author {
     private String lastName;
 
     private String biography;
-
+    @BuilderField
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
+    @BuilderField
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
+    @BuilderField
     public void setBiography(String biography) {
         this.biography = biography;
     }
